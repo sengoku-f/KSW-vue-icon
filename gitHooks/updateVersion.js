1daa301d4fc5ade1e9c9828306d68955ccd926a1
baobaomi900901 <baobaomi900901@icloud.com>
2024-06-05 18:57:10 +0800

    更新upv脚本, Bump, version to 2.0.30

--- gitHooks/updateVersion.js
+++ gitHooks/updateVersion.js
@@ -1,8 +1,9 @@
-// const fs = require('fs');
 import fs from 'fs'
+import {execSync} from 'child_process'
+
 const packageJsonPath = './package.json';
 
-// // 读取 package.json 文件
+// 读取 package.json 文件
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 
 // 获取当前版本号
@@ -21,3 +22,19 @@ fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 // 输出更新后的版本号
 console.log(`版本号已更新为 ${packageJson.version}`);
 
+// 获取 终端 输入的 字符串 用于填入 commit 信息
+const args = process.argv.slice(2)
+
+// 添加缓存
+execSync(`git add .`);
+
+// 运行 git commit -m "Bump version to ${packageJson.version}" 命令
+execSync(`git commit -m "${args}, Bump, version to ${packageJson.version}"`);
+
+execSync('git push origin main', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+});
