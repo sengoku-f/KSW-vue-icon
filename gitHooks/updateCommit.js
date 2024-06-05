edc013b4876cdefd2144cfd181005c0f751a2887
baobaomi900901 <baobaomi900901@icloud.com>
2024-06-05 11:21:49 +0800

    commit测试Bump version to 2.0.26

--- gitHooks/updateCommit.js
+++ gitHooks/updateCommit.js
@@ -3,6 +3,8 @@ import fs from 'fs'
 import {execSync} from 'child_process'
 const packageJsonPath = './package.json';
 
+const args = process.argv.slice(2)
+
 // // 读取 package.json 文件
 const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 
@@ -11,4 +13,4 @@ const currentVersion = packageJson.version;
 
 // 运行 git commit -m "Bump version to ${packageJson.version}" 命令
 // const { execSync } = require('child_process'); 
-execSync(`git commit -m "Bump version to ${packageJson.version}"`);
+execSync(`git commit -m "${args}Bump version to ${packageJson.version}"`);
