22b1deb921551229f60062083557299f42f64410
sengoku-f <31064651+sengoku-f@users.noreply.github.com>
2024-06-07 20:19:28 +0800

    同时生成 ES 模块和 CommonJS 模块

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import { globSync } from 'glob';
 import path from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -48,30 +49,57 @@ const siteConfig = {
 //   }
 // }
 
+// 为了匹配多种路径模式，可以使用数组传递给 `globSync`
+const files = globSync(['src/index.js', 'src/icons/*.js']);
+const inputEntries = Object.fromEntries(
+  files.map(file => [
+    path.relative(
+      // 相对于 `src` 文件夹生成相对路径
+      'src',
+      file.slice(0, file.length - path.extname(file).length)
+    ),
+    // 使用 `fileURLToPath` 将文件路径转换为 URL 文件路径
+    fileURLToPath(new URL(file, import.meta.url))
+  ])
+);
+
 const packagesConfig = {
   ...baseConfig,
   build: {
-    // sourcemap: true,
-    rollupOptions: {
-      input: {
-        index: path.resolve(__dirname, './src/index.js'),
-        map: path.resolve(__dirname, './src/map.js'),
-        runtime: path.resolve(__dirname, './src/runtime/index.js'),
-      },
-      external: ['vue'],
-      preserveEntrySignatures: 'allow-extension',
-      output: {
-        dir: 'packages',
-        entryFileNames: '[name].js',
-        preserveModules: true,
-        globals: {
-          vue: 'Vue'
-        }
-      }
-    },
     outDir: 'packages',
     emptyOutDir: true,
-  }
+    // sourcemap: true,
+    rollupOptions: {
+      // input: {
+      //   index: path.resolve(__dirname, './src/index.js'),
+      //   // map: path.resolve(__dirname, './src/map.js'),
+      //   'runtime/index': path.resolve(__dirname, './src/runtime/index.js'),
+      // },
+      input: inputEntries,
+      external: ['vue'],
+      preserveEntrySignatures: 'strict',
+      output: [
+        {
+          format: 'es',
+          dir: 'packages/es',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          globals: {
+            vue: 'Vue'
+          }
+        },
+        {
+          format: 'cjs',
+          dir: 'packages/cjs',
+          entryFileNames: '[name].js',
+          preserveModules: true,
+          globals: {
+            vue: 'Vue'
+          }
+        }
+      ]
+    },
+  },
 }
 
 export default process.env.BUILD === 'packages' ? packagesConfig : siteConfig
