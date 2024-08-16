d0a23574a6b8f3a240193c293c570f9d5e4f98ca
sengoku-f <31064651+sengoku-f@users.noreply.github.com>
2024-08-16 14:32:27 +0800

    在 nextTick 回调中添加了一个检查 if (svgRef.value)，以确保 svgRef.value 已经存在，然后再绑定事件。避免 svgRef.value 为 null 时调用 addEventListener 导致的错误

--- src/svg/setting-animation.js
+++ src/svg/setting-animation.js
@@ -2,25 +2,27 @@ const svgRef = ref(null);
 const animation = ref(null);
 const isAnimating = ref(false); // 动画进行中的标志
 nextTick(() => {
-  const gsapAnimation = gsap.to(animation.value, {
-    rotation: 180,
-    duration: 0.8,
-    repeat: 0,
-    ease: "sine.inOut",
-    transformOrigin: "center",
-    paused: true,
-    onComplete: () => {
-      // 动画完成时的处理逻辑
-      isAnimating.value = false; // 标志设置为 false，表示动画已完成
-    }
-  });
-  // 鼠标移入事件处理函数
-  const handleMouseEnter = () => {
-    if (gsapAnimation && !isAnimating.value) {
-      isAnimating.value = true; // 标志设置为 true，表示动画正在进行
-      gsapAnimation.restart(); // 重新开始动画
-    }
-  };
-  // 绑定鼠标移入事件
-  svgRef.value.addEventListener('mouseenter', handleMouseEnter);
+  if (svgRef.value) {
+    const gsapAnimation = gsap.to(animation.value, {
+      rotation: 180,
+      duration: 0.8,
+      repeat: 0,
+      ease: "sine.inOut",
+      transformOrigin: "center",
+      paused: true,
+      onComplete: () => {
+        // 动画完成时的处理逻辑
+        isAnimating.value = false; // 标志设置为 false，表示动画已完成
+      },
+    });
+    // 鼠标移入事件处理函数
+    const handleMouseEnter = () => {
+      if (gsapAnimation && !isAnimating.value) {
+        isAnimating.value = true; // 标志设置为 true，表示动画正在进行
+        gsapAnimation.restart(); // 重新开始动画
+      }
+    };
+    // 绑定鼠标移入事件
+    svgRef.value.addEventListener("mouseenter", handleMouseEnter);
+  }
 });
