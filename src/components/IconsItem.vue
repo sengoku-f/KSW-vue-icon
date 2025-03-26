3ea5b8ce5020f18aad33a149e34281b824203ee0
sengoku-f <31064651+sengoku-f@users.noreply.github.com>
2025-03-26 12:11:17 +0800

    迁移项目图标到 base

--- src/components/IconsItem.vue
+++ src/components/IconsItem.vue
@@ -11,10 +11,16 @@
     @onSearchChange="updateSearchQuery"
   />
   <div class="flex mx-auto max-w-7xl gap-6 mt-6 mb-12 px-4 sm:px-6 lg:px-8">
-    <aside class="sidebar flex-shrink-0 w-56 overflow-auto overscroll-auto sticky top-[84px]">
+    <aside
+      class="sidebar flex-shrink-0 w-56 overflow-auto overscroll-auto sticky top-[84px]"
+    >
       <ul class="flex flex-col gap-1">
         <li v-for="category in categories" :key="category">
-          <div @click="changeCategory(category)" class="w-full px-3 py-2 rounded-lg cursor-pointer select-none text-slate-500 font-normal hover:bg-slate-50 transition-all text-sm" :class="{ 'is-active': selectedCategory === category }">
+          <div
+            @click="changeCategory(category)"
+            class="w-full px-3 py-2 rounded-lg cursor-pointer select-none text-slate-500 font-normal hover:bg-slate-50 transition-all text-sm"
+            :class="{ 'is-active': selectedCategory === category }"
+          >
             {{ category }}
           </div>
         </li>
@@ -32,10 +38,10 @@ import IconPreview from "./IconPreview.vue";
 // 从 map 中引入所有 icons 组件
 import { ProjectIconsMap } from "@/map";
 import { iconsDataBase } from "~/icons-base.js";
-import { iconsDataGuangfa } from "~/icons-guangfa.js";
-import { iconsDataKingAutometa } from "~/icons-KingAutometa.js";
+// import { iconsDataGuangfa } from "~/icons-guangfa.js";
+// import { iconsDataKingAutometa } from "~/icons-KingAutometa.js";
 
-const iconsData = [ ...iconsDataBase, ...iconsDataGuangfa, ...iconsDataKingAutometa ];
+const iconsData = [...iconsDataBase];
 
 // 基于 name 去重
 const uniqueIconsData = Array.from(
@@ -72,7 +78,7 @@ const selectedCategory = ref("全部");
 
 // 获取所有类别
 const categories = computed(() => {
-  const allCategories = iconsData.map(icon => icon.categoryCN);
+  const allCategories = iconsData.map((icon) => icon.categoryCN);
   return ["全部", ...new Set(allCategories)];
 });
 
@@ -80,22 +86,37 @@ const categories = computed(() => {
 const sortIcons = () => {
   let sortedIcons = [...extendedIconsData];
   if (sortBy.value === "date") {
-    sortedIcons.sort((a, b) => new Date(b.modifiedTime) - new Date(a.modifiedTime));
+    sortedIcons.sort(
+      (a, b) => new Date(b.modifiedTime) - new Date(a.modifiedTime)
+    );
   } else {
     sortedIcons.sort((a, b) => a.componentName.localeCompare(b.componentName));
   }
-  if (showColorIcons.value === 'color') {
-    sortedIcons = sortedIcons.filter((icon) => icon.componentName.includes("Color"));
-  } else if (showColorIcons.value === 'monochrome') {
-    sortedIcons = sortedIcons.filter((icon) => !icon.componentName.includes("Color"));
+  if (showColorIcons.value === "color") {
+    sortedIcons = sortedIcons.filter((icon) =>
+      icon.componentName.includes("Color")
+    );
+  } else if (showColorIcons.value === "monochrome") {
+    sortedIcons = sortedIcons.filter(
+      (icon) => !icon.componentName.includes("Color")
+    );
   }
   if (showAnimationIcons.value) {
-    sortedIcons = sortedIcons.filter((icon) => icon.componentName.includes("Animation"));
+    sortedIcons = sortedIcons.filter((icon) =>
+      icon.componentName.includes("Animation")
+    );
   }
   if (selectedCategory.value !== "全部") {
-    sortedIcons = sortedIcons.filter(icon => icon.categoryCN === selectedCategory.value);
+    sortedIcons = sortedIcons.filter(
+      (icon) => icon.categoryCN === selectedCategory.value
+    );
   }
-  icons.value = sortedIcons.map((icon) => ({ componentName: icon.componentName, title: icon.title, projectName: icon.projectName, isAlias: icon.isAlias }));
+  icons.value = sortedIcons.map((icon) => ({
+    componentName: icon.componentName,
+    title: icon.title,
+    projectName: icon.projectName,
+    isAlias: icon.isAlias,
+  }));
 };
 
 // 初始排序
@@ -134,7 +155,9 @@ const changeCategory = (category) => {
 
 // 计算属性用于根据搜索框输入过滤图标列表
 const filteredIcons = computed(() => {
-  return icons.value.filter((icon) => icon.componentName.toLowerCase().includes(searchQuery.value.toLowerCase()));
+  return icons.value.filter((icon) =>
+    icon.componentName.toLowerCase().includes(searchQuery.value.toLowerCase())
+  );
 });
 </script>
 
--- src/map.js
+++ src/map.js
@@ -1,9 +1,5 @@
 import * as Base from "./icons/base";
-import * as Guangfa from "./icons/guangfa";
-import * as KingAutometa from "./icons/KingAutometa";
 
 export const ProjectIconsMap = {
   Base,
-  Guangfa,
-  KingAutometa,
 };
